f707282a8fcdbb4a9f57d76888e5e38bc2cf802c
finde <finde.xumara@gmail.com>
2015-02-12 07:24:28 +0100

    clean up + update assignment.sh

--- assignment-1/assignment_1.sh
+++ assignment-1/assignment_1.sh
@@ -2,6 +2,7 @@
 
 treebank=$1
 number_of_sentences=$2
+bitpar='../BitPar/src/bitpar'
 
 echo "::Preparation::"
 echo " - limiting treebanks to $number_of_sentences sentences"
@@ -23,7 +24,7 @@ sed -i '' 's/"//g' _treebanks.grammar.bitpar
 
 # Do a single pass of io with bitpar
 echo " - do a single pass of io with bitpar"
-bitpar -s TOP _treebanks.grammar.bitpar _treebanks.lexicon.bitpar _corpus -em em
+$bitpar -s TOP _treebanks.grammar.bitpar _treebanks.lexicon.bitpar _corpus -em em
 
 # # remove lines with 0 counts
 echo " - remove empty lines"
@@ -31,14 +32,15 @@ less em.gram | grep -v '0\.00' > em.grammar
 less em.lex | grep -v '0\.000*' | awk '{print $1"\t"$2" "$3 }' > em.lexicon
 
 echo " - parse again"
-bitpar -s TOP em.grammar em.lexicon _corpus -b 1 > _parsed
+$bitpar -s TOP em.grammar em.lexicon _corpus -b 1 > _parsed
 cat _parsed | sed -i '' 's/\\//g' _parsed
 
 echo " - running evalC"
 java -jar ../evalC/evalC.jar new_treebank_gold _parsed _eval
 
-# # get probabilities and write to file $output
-# cat $parse_forests | grep -E -o '^[{]*\(TOP=\#i\[P=[0-9]+\.[0-9]*[-e]*[0-9]*\]' | grep -E -o '[0-9]+\.[0-9]*[-e]*[0-9]*' > $output
+# # # get probabilities and write to file $output
+# $bitpar -vp -s TOP em.grammar em.lexicon _corpus -b 1 > _parsed_proba
+# cat _parsed_proba | grep -E -o '^[{]*\(TOP=\#i\[P=[0-9]+\.[0-9]*[-e]*[0-9]*\]' | grep -E -o '[0-9]+\.[0-9]*[-e]*[0-9]*' > _output
 
 # # sum to get log-likelihood from file $output
-# python sum_log_probabilities $output
+# python ../sum_log_probabilities.py _output
